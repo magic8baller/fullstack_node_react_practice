9326b033742b3aebc012ead4dd3d95b3f7f90a63
magic8baller <lturis@gmail.com>
2019-03-03 18:37:04 -0500

    reorg codebase

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const GenerationEngine = require('./engine');
+const GenerationEngine = require('./generation/engine');
 
 const app = express();
 const engine = new GenerationEngine();
-const port = 4001;
 
 engine.start();
 
@@ -11,4 +10,4 @@ app.get('/dragon/new', (req, res) => {
   res.json({ dragon: engine.generation.newDragon() });
 });
 
-app.listen(port, () => console.log(`listening on ${port}`));
+module.exports = app;
---
+++ backend/bin/server.js
@@ -0,0 +1,5 @@
+const app = require('../app/index');
+
+const port = 4001;
+
+app.listen(port, () => console.log(`listening on ${port}`));
