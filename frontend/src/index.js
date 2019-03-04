3dcaa8f3198d6d6118e87a2759eae757a2cda328
magic8baller <lturis@gmail.com>
2019-03-04 18:12:58 -0500

    add HMR

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 render(
-  <div>Dragon Stack from reaaccckkt</div>,
+  <div>Dragon Stack from reaaccckkt is Hot</div>,
   document.getElementById('root')
 );
