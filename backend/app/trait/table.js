48ddd1298eeb789994d565a899e7082a1dcc9789
magic8baller <lturis@gmail.com>
2019-03-04 14:27:13 -0500

    create getTraitId fn and successful test!

---
+++ backend/app/trait/table.js
@@ -0,0 +1,20 @@
+const pool = require('../../databasePool');
+class TraitTable {
+  static getTraitId({ traitType, traitValue }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'SELECT id FROM trait WHERE "traitType" = $1 AND "traitValue" = $2',
+        [traitType, traitValue],
+        (err, res) => {
+          if (err) return reject(err);
+          resolve({ traitId: res.rows[0].id });
+        }
+      );
+    });
+  }
+}
+// test
+TraitTable.getTraitId({ traitType: 'backgroundColor', traitValue: 'green' })
+  .then(({ traitId }) => console.log('traitId', traitId))
+  .catch(err => console.error('error', err));
+module.exports = TraitTable;
