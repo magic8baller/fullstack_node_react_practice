24d8d8a73e49bb625a8b64c9e6729b949e9de41b
magic8baller <lturis@gmail.com>
2019-03-05 15:46:08 -0500

    more redux arch

--- frontend/src/actions/generation.js
+++ frontend/src/actions/generation.js
@@ -1,7 +1,7 @@
-import {GENERATION_ACTION_TYPE} from './types'
-const generationActionCreator = payload => {
+import { GENERATION_ACTION_TYPE } from './types';
+export const generationActionCreator = payload => {
   return {
     type: GENERATION_ACTION_TYPE,
     generation: payload
-  }
-}
+  };
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { generationActionCreator } from './actions/generation';
 import Dragon from './components/Dragon';
 import Generation from './components/Generation';
 import './index.css';
+import { generationReducer } from './reducers/index';
+
 if (module.hot) {
   module.hot.accept();
 }
+
+const store = createStore(
+  generationReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.subscribe(() => console.log('store state update', store.getState()));
+
+fetch('http://localhost:4001/generation')
+  .then(r => r.json())
+  .then(json => {
+    store.dispatch(generationActionCreator(json.generation));
+  });
 
 render(
   <div>
