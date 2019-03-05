45ed62d9a8d3ccbb6607112137a711f5afabe78f
magic8baller <lturis@gmail.com>
2019-03-05 14:53:15 -0500

    exploring redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
 import Dragon from './components/Dragon';
 import Generation from './components/Generation';
 import './index.css';
-if (module.hot) {
-  module.hot.accept();
-}
+const DEFAULT_GENERATION = { generationId: '', expiration: '' };
+const GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE';
+const generationReducer = (state, action) => {
+  if (action.type === GENERATION_ACTION_TYPE) {
+    return { generation: action.generation };
+  }
+  return {
+    generation: DEFAULT_GENERATION
+  };
+};
 
+const store = createStore(generationReducer);
+
+store.subscribe(() => console.log('store state update', store.getState()));
+
+console.log('store', store);
+store.dispatch({
+  type: GENERATION_ACTION_TYPE,
+  generation: { generationId: 'goo', expiration: 20 }
+});
+console.log('store.getState()', store.getState());
+
+//wrapper AROUND action
+const generationActionCreator = payload => {
+  return {
+    type: GENERATION_ACTION_TYPE,
+    generation: payload
+  };
+};
+const zooAction = generationActionCreator({
+  generationId: 'zoo',
+  expiration: 'bar'
+});
+
+store.dispatch(zooAction);
+
+fetch('http://localhost:4001/generation')
+  .then(r => r.json())
+  .then(json => {
+    store.dispatch(generationActionCreator(json.generation));
+  });
 render(
   <div>
     <h2>Dragon Stack</h2>
     <Generation />
     <Dragon />
   </div>,
+
   document.getElementById('root')
 );
