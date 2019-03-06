72a7b19d260aeeaed2ed4ec72266cf217fc661ae
magic8baller <lturis@gmail.com>
2019-03-05 21:41:17 -0500

    create account table and add to config script

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -5,6 +5,7 @@ echo "Configuring dragonstackdb"
 dropdb -U dragon_user dragonstackdb
 createdb -U dragon_user dragonstackdb
 
+psql -U dragon_user dragonstackdb < ./bin/sql/account.sql
 psql -U dragon_user dragonstackdb < ./bin/sql/generation.sql
 psql -U dragon_user dragonstackdb < ./bin/sql/dragon.sql
 psql -U dragon_user dragonstackdb < ./bin/sql/trait.sql
