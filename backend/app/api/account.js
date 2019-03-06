dac1252bfbe26b34a931e4fca2bbc7f00e2ea078
magic8baller <lturis@gmail.com>
2019-03-06 15:18:51 -0500

    remove comments

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -8,22 +8,19 @@ router.post('/signup', (req, res, next) => {
   const usernameHash = hash(username);
   const passwordHash = hash(password);
 
-  // first get account to check if account exists before deciding to store as 'new'
   AccountTable.getAccount({ usernameHash })
     .then(({ account }) => {
-      //if account is undef
       if (!account) {
         return AccountTable.storeAccount({ usernameHash, passwordHash });
       } else {
         const error = new Error('This account already exists');
-        //conflict with existing data
+
         error.statusCode = 409;
-        // next(error);
+
         throw error;
       }
     })
-    //chain down here instead of leaving with storeAccount - bc result of .then handler itself
-    //returning promise to outer scope
+
     .then(() => res.json({ message: 'SUCCE$$$$$$$$FUL account storage' }))
     .catch(error => next(error));
 });
