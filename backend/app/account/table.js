58946a784cf020426100826bedec6a7200d25f9e
magic8baller <lturis@gmail.com>
2019-03-06 15:18:01 -0500

    avoid duplicate account signup and create getAccount fn, error on dupes, cleaned up api/account by chaining promises...

--- backend/app/account/table.js
+++ backend/app/account/table.js
@@ -14,6 +14,21 @@ class AccountTable {
       );
     });
   }
+
+  static getAccount({ usernameHash }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `SELECT id, "passwordHash" FROM account
+        WHERE "usernameHash" = $1`,
+        [usernameHash],
+        (err, res) => {
+          if (err) return reject(err);
+
+          resolve({ account: res.rows[0] });
+        }
+      );
+    });
+  }
 }
 
 module.exports = AccountTable;
--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -8,9 +8,24 @@ router.post('/signup', (req, res, next) => {
   const usernameHash = hash(username);
   const passwordHash = hash(password);
 
-  AccountTable.storeAccount({ usernameHash, passwordHash })
+  // first get account to check if account exists before deciding to store as 'new'
+  AccountTable.getAccount({ usernameHash })
+    .then(({ account }) => {
+      //if account is undef
+      if (!account) {
+        return AccountTable.storeAccount({ usernameHash, passwordHash });
+      } else {
+        const error = new Error('This account already exists');
+        //conflict with existing data
+        error.statusCode = 409;
+        // next(error);
+        throw error;
+      }
+    })
+    //chain down here instead of leaving with storeAccount - bc result of .then handler itself
+    //returning promise to outer scope
     .then(() => res.json({ message: 'SUCCE$$$$$$$$FUL account storage' }))
-    .catch(err => next(err));
+    .catch(error => next(error));
 });
 
 module.exports = router;
