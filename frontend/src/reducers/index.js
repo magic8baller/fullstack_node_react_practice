c7e4fb2ee77c8c0ee59235928fe47516aad629ae
magic8baller <lturis@gmail.com>
2019-03-06 19:24:41 -0500

    create account actions, account reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import account from './account';
 import dragon from './dragon';
 import generation from './generation';
 
-export default combineReducers({ generation, dragon });
+export default combineReducers({ account, dragon, generation });
